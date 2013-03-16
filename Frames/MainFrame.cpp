ad6c5b9a415d7713595d9feba50d5f032a0015f6
Mike Sherwood <coolhand2@gmail.com>
2013-03-16 04:26:35 -0600

    Finishing up the basics
    Added all the file loading, saving, and closing junk for the file editor
    portion. Also cleaned up the makefile for pretty output.

--- Frames/MainFrame.cpp
+++ Frames/MainFrame.cpp
@@ -6,6 +6,9 @@
  */
 
 #include "MainFrame.h"
+#include <sstream>
+#include <iostream>
+#include <wx/filedlg.h>
 
 BEGIN_EVENT_TABLE(MainFrame, wxFrame)
     EVT_MENU(M_New, MainFrame::newFile)
@@ -18,17 +21,21 @@ END_EVENT_TABLE()
 
 MainFrame::MainFrame(const wxString &title, const wxPoint &position, const wxSize &size)
 : wxFrame((wxFrame *) NULL, -1, title, position, size){
-    _text = new wxTextCtrl(this, 0, _("Hello, World!"), wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_RICH, wxDefaultValidator, wxTextCtrlNameStr);
-    _menu = new wxMenuBar();
-    SetMenuBar(_menu);
-    CreateStatusBar(2);
-
-    _populateMenu();
-
+    _setupFrame();
+    _loadFrame();
 }
 
-void MainFrame::_populateMenu(){
+void MainFrame::_setupFrame(){
+    CreateStatusBar(2);
+}
 
+void MainFrame::_loadFrame(){
+    _loadMenus();
+    _text = new wxTextCtrl(this, 0, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_RICH, wxDefaultValidator, wxTextCtrlNameStr);
+}
+
+void MainFrame::_loadMenus(){
+    wxMenuBar *menu = new wxMenuBar();
     wxMenu *fileMenu = new wxMenu();
 
     fileMenu->Append(M_New, _("&New"), _("Create New File"));
@@ -40,29 +47,65 @@ void MainFrame::_populateMenu(){
     fileMenu->AppendSeparator();
     fileMenu->Append(M_Quit, _("&Quit"), _("Close The Application"));
 
-    _menu->Append(fileMenu, _("&File"));
+    menu->Append(fileMenu, _("&File"));
+    SetMenuBar(menu);
+}
+
+wxString MainFrame::_getFileTypes(){
+    std::stringstream ss;
+    ss << "Text Files (*.txt)|*.txt|"
+       << "C++ Source Files (*.cpp, *.cxx, *.cc, *.c)|*.cpp;*.cxx;*.cc;*.c|"
+       << "C++ Header Files (*.hpp, *.hxx, *.hh, *.h)|*.hpp;*.hxx;*.hh;*.h|"
+       << "C Source Files (*.c)|*.c|"
+       << "C Header Files (*.h)|*.h|"
+       << "PHP Source Files (*.php)|*.php|"
+       << "Java Source Files (*.java)|*.java|"
+       << "XML Files (*.xml)|*.xml";
+    std::string str = ss.str();
+    wxString wxs(str.c_str(), wxConvUTF8);
+    return wxs;
 }
 
 void MainFrame::newFile(wxCommandEvent& event){
-
+    _text->Clear();
+    _documentPath = _("");
+    SetTitle(_("Edit - New *"));
 }
 
 void MainFrame::openFile(wxCommandEvent& event){
-
+    wxFileDialog dialog(this, _("Choose A File To Open"), wxEmptyString, wxEmptyString, _getFileTypes(), wxFD_OPEN, wxDefaultPosition);
+    if(dialog.ShowModal() == wxID_OK){
+        _documentPath = dialog.GetPath();
+        _text->LoadFile(_documentPath);
+        SetTitle(wxString(_("Edit - ")) << dialog.GetFilename());
+    }
 }
 
 void MainFrame::saveFile(wxCommandEvent& event){
-
+    if( !_documentPath.IsEmpty() ) {
+        _text->SaveFile(_documentPath);
+    } else {
+        saveAs(event);
+    }
 }
 
 void MainFrame::saveAs(wxCommandEvent& event){
-
+    wxFileDialog dialog(this, _("Save File As"), wxEmptyString, wxEmptyString, _getFileTypes(), wxFD_SAVE| wxFD_OVERWRITE_PROMPT, wxDefaultPosition);
+    if(dialog.ShowModal() == wxID_OK){
+        _documentPath = dialog.GetPath();
+        _text->SaveFile(_documentPath);
+        SetTitle(wxString(_("Edit - ")) << dialog.GetFilename());
+    }
 }
 
 void MainFrame::closeFile(wxCommandEvent& event){
-
+    if( _text->IsModified() ){
+        saveFile(event);
+    }
+    newFile(event);
 }
 
 void MainFrame::quit(wxCommandEvent &event){
+    closeFile(event);
     Close(TRUE);
 }
--- Makefile
+++ Makefile
@@ -1,23 +1,28 @@
 WXLIBS = `wx-config --libs`
 WXFLAGS = `wx-config --cxxflags`
 INCLUDES = -I. -I./App -I./Frames
-LIBS =
-FLAGS = -g $(INCLUDES)
+LIBS = $(WXLIBS)
+ifeq ($(BUILD), release)
+    FLAGS = -g -Wall -O3 -foptimize-sibling-calls $(INCLUDES) $(WXFLAGS)
+else
+    FLAGS = -g3 -fno-inline -O0 $(INCLUDES) $(WXFLAGS)
+endif
 CPP = g++
+QUIET_CPP = @echo '    ' G++ $@;
+QUIET_LINK = @echo '    ' LINK $@;
+QUIET_RM = @echo '    ' CLEAN;
 
 .SUFFIXES:  .cpp
-
 .cpp.o:
-	$(CPP) $(WXFLAGS) $(FLAGS) -c $< -o $@
-
-.cpp :
-	$(CPP) $(WXFLAGS) $(FLAGS) $< -o $@ $(WXLIBS) $(LIBS)
+	$(QUIET_CPP)$(CPP) $(FLAGS) -c $< -o $@
 
 SRC = main.cpp App/wxEditor.cpp Frames/MainFrame.cpp
 OBJ = $(addsuffix .o, $(basename $(SRC)))
 
-all: $(OBJ)
-	$(CPP) $(WXFLAGS) $(FLAGS) -o $@ $(OBJ) $(WXLIBS) $(LIBS)
+all: wxEditor clean
+
+wxEditor: $(OBJ)
+	$(QUIET_LINK)$(CPP) $(FLAGS) -o $@ $(OBJ) $(LIBS)
 
 clean:
-	rm -f $(OBJ)
+	$(QUIET_RM) rm -f $(OBJ)
